3c10731c9adb2b21e06c4f51ac23d3edc0a2dbd7
alexbulanovgoodmail <alexbulanovgoodmail@gmail.com>
2024-03-19 15:14:20 +0300

    Typing Reactive Data

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,18 @@
 import TheHeader from "@/components/TheHeader.vue";
 import EntryEditor from "./components/EntryEditor.vue";
 import EntryCard from "@/components/EntryCard.vue";
+
+import type User from "./types/User";
+
+import { reactive } from "vue";
+
+const user: User = reactive({
+  id: 1,
+  username: "alex-bulanov_io",
+  setting: [],
+});
+
+console.log("user", user.id);
 </script>
 
 <template>
