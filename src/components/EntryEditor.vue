0cf0cd088c22f430c70ed298962ceeb9c59243a9
alexbulanovgoodmail <alexbulanovgoodmail@gmail.com>
2024-03-19 16:22:21 +0300

    Typing Event Handlers

--- src/components/EntryEditor.vue
+++ src/components/EntryEditor.vue
@@ -12,17 +12,32 @@ const text = ref("");
 const emoji = ref<Emoji | null>(null);
 
 const charCount = computed<number>(() => text.value.length);
+
+const maxChars = 280;
+
+const handleTextInput = (event: Event) => {
+  const textarea = event.target as HTMLTextAreaElement;
+
+  if (textarea.value.length <= maxChars) {
+    text.value = textarea.value;
+  } else {
+    text.value = textarea.value = textarea.value.substring(0, maxChars);
+  }
+
+  console.log("textarea", textarea);
+};
 </script>
 
 <template>
   <form class="entry-form" @submit.prevent>
     <textarea
-      v-model="text"
+      :value="text"
       placeholder="New Journal Entry for danielkelly_io"
+      @keyup="handleTextInput"
     ></textarea>
     <EmojiField v-model="emoji" />
     <div class="entry-form-footer">
-      <span>{{ charCount }} / 280</span>
+      <span>{{ charCount }} / {{ maxChars }}</span>
       <button>Remember <ArrowCircleRight width="20" /></button>
     </div>
   </form>
