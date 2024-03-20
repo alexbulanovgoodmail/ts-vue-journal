5623167661cfce170839a1fbea9fac75e134be35
alexbulanovgoodmail <alexbulanovgoodmail@gmail.com>
2024-03-20 17:22:38 +0300

    Declaring and Typing Component Props

--- src/components/EntryCard.vue
+++ src/components/EntryCard.vue
@@ -2,16 +2,24 @@
 import DateDisplay from "./DateDisplay.vue";
 import UseEmojis from "@/composables/UseEmojis";
 const { findEmoji } = UseEmojis();
+
+import type Entry from "@/types/Entry";
+
+interface Props {
+  entry: Entry;
+}
+
+const props = defineProps<Props>();
 </script>
 
 <template>
   <div class="entry-card">
     <div class="entry-card-body">
-      <component width="75" :is="findEmoji('happy')"></component>
-      <div class="entry-text">Today I enjoyed walking the dog in the park.</div>
+      <component width="75" :is="findEmoji(entry.emoji)"></component>
+      <div class="entry-text">{{ entry.body }}</div>
     </div>
     <div class="entry-footer">
-      <DateDisplay :date="new Date()" class="mr-2" />
+      <DateDisplay :date="entry.createdAt" class="mr-2" />
       |
       <span class="ml-2">danielkelly_io</span>
     </div>
